62619e8f93a242756e5e9458410c0cfa25b3b44b
humberto-silvah <humbertonoite@gmail.com>
2025-08-22 19:26:09 -0300

    Atualizando

--- questao2.py
+++ questao2.py
@@ -1,13 +1,10 @@
 def teste_polindromo(item):
-    frase = item.replace(
-        " ", ""
-    ).lower()  # tratamento de erro para remover espaço em branco e pcaps
+    frase = item.replace(" ", "").lower() #tratamento de erro para remover espaço em branco e pcaps
 
     if frase == frase[::-1]:
         print("É Polindromo")
     else:
         print("Não é Polimbromo")
-
 
 # palavra = input("Digite uma palavra ou frase: ")
 palavra = "Socorram me subino onibus em marrocos"
